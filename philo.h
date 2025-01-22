ba0ef108c5e776b9fa37f99d0759e19a93aff19b
KarmaFaber <maria.zolotarova@gmail.com>
2025-01-22 11:55:05 +0100

    inicio con main

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mzolotar <mzolotar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 11:08:11 by mzolotar          #+#    #+#             */
-/*   Updated: 2025/01/22 11:30:19 by mzolotar         ###   ########.fr       */
+/*   Updated: 2025/01/22 11:45:32 by mzolotar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,18 @@
 # include <sys/time.h> 	//gettimeofday
 # include <pthread.h> 	//pthread_create, pthread_detach, pthread_join, pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
 
-/*#◉───▣───▢◇▢───▣───◉•◉───▣───▢ Main code: ▢───▣───◉•◉───▣───▢◇▢───▣───◉#*/
+//#➵⤐──╌╌➣⋆➣╌╌──⤏➵•➵⤐──╌╌➣⋆➣╌╌──Macros  :──╌╌➣⋆➣╌╌──⤏➵•➵⤐──╌╌➣⋆➣╌╌➔#
+//# define 
+
+//#➵⤐──╌╌➣⋆➣╌─⤏➵•➵⤐──╌╌➣⋆➣╌╌──Structures  :──╌╌➣⋆➣╌╌⤏➵•➵⤐──╌╌➣⋆➣╌╌➔#
 
 
 
-/*#◉───▣───▢◇▢───▣───◉•◉───▣───▢ Utils: ▢───▣───◉•◉───▣───▢◇▢───▣───◉#*/
+//#➵⤐╌╌➣⋆➣╌─⤏➵•➵⤐╌╌➣⋆➣╌╌─Function Prototypes : :──➣⋆➣╌⤏➵•➵⤐╌╌➣⋆➣╌╌➔#
+//main.c (/5):
+
+
+
 
 
 #endif
